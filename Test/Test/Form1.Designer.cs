2df3d1a4cb3efd1541861e278e6e1e73485b988d
k-zimm <kyle.zimmerii@mymail.nwtc.edu>
2018-08-28 19:39:52 -0500

    Accidental change

--- Test/Test/Form1.Designer.cs
+++ Test/Test/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(425, 261);
             this.Name = "Form1";
             this.Text = "Test Project";
             this.ResumeLayout(false);
